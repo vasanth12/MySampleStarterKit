6173cda7a979eff9e648ea31a2511adb1286a31e
vasanth kumar r <vasanth3422@gmail.com>
2017-10-01 21:46:25 +0530

    myfirstcomitt

---
+++ client/components/chip.js
@@ -0,0 +1,7 @@
+// import React,{Component} from 'react';
+// import PropTypes from 'prop-types';
+// import ReactDOM from 'react-dom';
+// import _ from 'lodash';
+// import '../components/css/Form.css';
+//
+// // https://stackoverflow.com/questions/44279355/reactjs-transferring-the-data-between-two-componen
